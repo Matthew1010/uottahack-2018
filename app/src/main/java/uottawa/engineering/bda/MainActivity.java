8bc515d631a63bfa581227aae37be05044105c18
Matthew Berchan <MatthewBerchan@Matthews-MacBook-Pro.local>
2018-02-18 11:58:17 -0500

    Phase1

--- app/src/main/java/uottawa/engineering/bda/MainActivity.java
+++ app/src/main/java/uottawa/engineering/bda/MainActivity.java
@@ -1,13 +1,25 @@
 package uottawa.engineering.bda;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.barcode.Barcode;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    Button scanbtn;
+    TextView result;
+    public static final int REQUEST_CODE = 100;
+    public static final int PERMISSION_REQUEST = 200;
 
 
     @Override
@@ -15,8 +27,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View btn1 = (View) findViewById(R.id.btn1);
+        scanbtn = (Button) findViewById(R.id.scanbtn);
+        result = (TextView) findViewById(R.id.result);
 
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST);
+        }
+        scanbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Scan.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            if(data != null){
+                final Barcode barcode = data.getParcelableExtra("barcode");
+                result.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        result.setText(barcode.displayValue);
+                    }
+                });
+            }
+        }
+    }
+}
+
+
+
+
+
+        /*
+        View btn1 = (View) findViewById(R.id.btn1);
         btn1.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -26,11 +73,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-
-    }
-
+        */
 
 
 
 
-}
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/uottawa/engineering/bda/Scan.java
+++ app/src/main/java/uottawa/engineering/bda/Scan.java
@@ -3,17 +3,22 @@ package uottawa.engineering.bda;
 import android.*;
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Vibrator;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.view.SurfaceView;
 import android.graphics.Camera;
+import android.widget.Toast;
 
 
 import com.google.android.gms.vision.CameraSource;
@@ -26,11 +31,92 @@ import java.io.IOException;
 
 public class Scan extends AppCompatActivity {
 
+    SurfaceView cameraView;
+    BarcodeDetector barcode;
+    CameraSource cameraSource;
+    SurfaceHolder holder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+
+
+        cameraView = (SurfaceView) findViewById(R.id.cameraView);
+        cameraView.setZOrderMediaOverlay(true);
+        holder = cameraView.getHolder();
+        barcode = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+        if(!barcode.isOperational()){
+            Toast.makeText(getApplicationContext(), "Sorry, Couldn't setup the detector", Toast.LENGTH_LONG).show();
+            this.finish();
+        }
+        cameraSource = new CameraSource.Builder(this, barcode)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedFps(24)
+                .setAutoFocusEnabled(true)
+                .setRequestedPreviewSize(1920,1024)
+                .build();
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try{
+                    if(ContextCompat.checkSelfPermission(Scan.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                        cameraSource.start(cameraView.getHolder());
+                    }
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+        barcode.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes =  detections.getDetectedItems();
+                if(barcodes.size() > 0){
+                    Intent intent = new Intent();
+                    intent.putExtra("barcode", barcodes.valueAt(0));
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            }
+        });
+    }
+}
+
+
+
+    /*
+
     SurfaceView cameraPreview;
     TextView txtResult;
     BarcodeDetector barcodeDetector;
     CameraSource cameraSource;
     final int RequestCameraPermissionID = 1001;
+
+    Button scanbtn;
+    TextView result;
+    public static final int REQUEST_CODE = 100;
+    public static final int PERMISSION_REQUEST = 200;
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -56,6 +142,48 @@ public class Scan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        /*
+        scanbtn = (Button) findViewById(R.id.scanbtn);
+        result = (TextView) findViewById(R.id.txtResult);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST);
+        }
+        scanbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Scan.this, ScanActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                final Barcode barcode = data.getParcelableExtra("barcode");
+                result.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        result.setText(barcode.displayValue);
+                    }
+                });
+            }
+        }
+
+
+*/
+
+
+
+
+
+
+
+
+        //########################################################################
+        /*
 
 
         cameraPreview = (SurfaceView) findViewById(R.id.cameraPreview);
@@ -168,3 +296,4 @@ public class Scan extends AppCompatActivity {
 
 
 }
+*/
