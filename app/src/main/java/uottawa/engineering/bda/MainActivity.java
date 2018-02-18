2d327cbe69b80fb04017ae1e79ed3ad1b70e0136
Matthew Berchan <MatthewBerchan@Matthews-MacBook-Pro.local>
2018-02-17 22:32:57 -0500

    Phase1

--- app/src/main/java/uottawa/engineering/bda/MainActivity.java
+++ app/src/main/java/uottawa/engineering/bda/MainActivity.java
@@ -2,6 +2,9 @@ package uottawa.engineering.bda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View btn1 = (View) findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getApplicationContext(), Scan.class);
+                startActivity(i);
+            }
+        });
+
     }
+
+
+
+
 
 }
